a78721413ef3dc819708e899e781b588bd4c7f40
rodrigo826 <58783821+rodrigo826@users.noreply.github.com>
2021-04-16 18:18:05 -0400

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ import {
 	Vector3,
 	WebGLRenderer,
 	sRGBEncoding,
-  } from 'three';
+  } from '../src/three.js';';
 
 import { GUI } from 'dat.gui';
 import { createBackground } from '../lib/three-vignette.js';
