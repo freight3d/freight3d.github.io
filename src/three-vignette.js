58de13701631b003145a6a2024bd63d3d1d6c7fe
rodrigo826 <58783821+rodrigo826@users.noreply.github.com>
2021-04-19 14:05:39 -0400

    Add files via upload

--- src/three-vignette.js
+++ src/three-vignette.js
@@ -2,41 +2,31 @@
  * Source: https://github.com/mattdesl/three-vignette-background
  * License: MIT
  */
-
-import {
-  Color,
-  DoubleSide,
-  Mesh,
-  PlaneGeometry,
-  RawShaderMaterial,
-  Vector2
-} from '.three';
-
 import vert from './three-vignette.vert';
 import frag from './three-vignette.frag';
 
 function createBackground (opt) {
   opt = opt || {}
-  var geometry = opt.geometry || new PlaneGeometry(2, 2, 1)
-  var material = new RawShaderMaterial({
+  var geometry = opt.geometry || new THREE.PlaneGeometry(2, 2, 1)
+  var material = new THREE.RawShaderMaterial({
     vertexShader: vert,
     fragmentShader: frag,
-    side: DoubleSide,
+    side: THREE.DoubleSide,
     uniforms: {
       aspectCorrection: { type: 'i', value: false },
       aspect: { type: 'f', value: 1 },
       grainScale: { type: 'f', value: 0.005 },
       grainTime: { type: 'f', value: 0 },
       noiseAlpha: { type: 'f', value: 0.25 },
-      offset: { type: 'v2', value: new Vector2(0, 0) },
-      scale: { type: 'v2', value: new Vector2(1, 1) },
-      smooth: { type: 'v2', value: new Vector2(0.0, 1.0) },
-      color1: { type: 'c', value: new Color('#fff') },
-      color2: { type: 'c', value: new Color('#283844') }
+      offset: { type: 'v2', value: new THREE.Vector2(0, 0) },
+      scale: { type: 'v2', value: new THREE.Vector2(1, 1) },
+      smooth: { type: 'v2', value: new THREE.Vector2(0.0, 1.0) },
+      color1: { type: 'c', value: new THREE.Color('#fff') },
+      color2: { type: 'c', value: new THREE.Color('#283844') }
     },
     depthTest: false
   })
-  var mesh = new Mesh(geometry, material)
+  var mesh = new THREE.Mesh(geometry, material)
   mesh.frustumCulled = false
   mesh.style = style
   if (opt) mesh.style(opt)
@@ -47,7 +37,7 @@ function createBackground (opt) {
     if (Array.isArray(opt.colors)) {
       var colors = opt.colors.map(function (c) {
         if (typeof c === 'string' || typeof c === 'number') {
-          return new Color(c)
+          return new THREE.Color(c)
         }
         return c
       })
@@ -64,11 +54,11 @@ function createBackground (opt) {
       material.uniforms.grainTime.value = opt.grainTime
     }
     if (opt.smooth) {
-      var smooth = fromArray(opt.smooth, Vector2)
+      var smooth = fromArray(opt.smooth, THREE.Vector2)
       material.uniforms.smooth.value.copy(smooth)
     }
     if (opt.offset) {
-      var offset = fromArray(opt.offset, Vector2)
+      var offset = fromArray(opt.offset, THREE.Vector2)
       material.uniforms.offset.value.copy(offset)
     }
     if (typeof opt.noiseAlpha === 'number') {
@@ -79,7 +69,7 @@ function createBackground (opt) {
       if (typeof scale === 'number') {
         scale = [ scale, scale ]
       }
-      scale = fromArray(scale, Vector2)
+      scale = fromArray(scale, THREE.Vector2)
       material.uniforms.scale.value.copy(scale)
     }
     if (typeof opt.aspectCorrection !== 'undefined') {
